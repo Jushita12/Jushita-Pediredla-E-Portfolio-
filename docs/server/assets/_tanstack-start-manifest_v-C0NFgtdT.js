aa7fa41a11dea65e741b92d77b506c967487238a
Your Name <your-email@example.com>
2026-06-10 12:27:22 +0800

    Rebuild site with Vite base and copy client output to docs for GitHub Pages

---
+++ docs/server/assets/_tanstack-start-manifest_v-C0NFgtdT.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Jushita/APU/HKRKOM SEMESTER 8/Engineer in Society/Portfolio Version 2 -- Final June 9th/src/routes/__root.tsx", children: ["/"], preloads: ["/Jushita-Pediredla-E-Portfolio/assets/index-ljRiEYaB.js"], scripts: [{ attrs: { type: "module", async: true, src: "/Jushita-Pediredla-E-Portfolio/assets/index-ljRiEYaB.js" } }] }, "/": { filePath: "D:/Jushita/APU/HKRKOM SEMESTER 8/Engineer in Society/Portfolio Version 2 -- Final June 9th/src/routes/index.tsx", children: void 0, preloads: ["/Jushita-Pediredla-E-Portfolio/assets/index-tkQI3j58.js"] } } });
+export {
+  tsrStartManifest
+};
